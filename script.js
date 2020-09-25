1f6d7b1edb09bd16694d83e608d5a3a17ff7e0c5
Roberto Nava <a320832@uach.mx>
2020-09-25 12:16:41 -0600

    act8

---
+++ script.js
@@ -0,0 +1,11 @@
+function registro(name,sex){
+if(var2==='femenino'){
+    console.log(`Bienvenida ${var1}`);
+}else{
+    console.log(`Bienvenido ${var1}`);
+}
+
+}
+name= document.getElementById('var1');
+sex= document.getElementById('var2');
+registro(var1,var2);
